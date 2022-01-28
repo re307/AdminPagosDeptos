85e01ac8b0c729fe3ca2283f54603fee0229aa7e
Rafael Eduardo Méndez <raed307@gmail.com>
2022-01-27 21:07:07 -0600

    APD - Uso de ls BD
    Se realiza primera consulta a la base de datos por medio de mongoose.

--- server/src/Controlles/adminPagos.controller.js
+++ server/src/Controlles/adminPagos.controller.js
@@ -1,6 +1,18 @@
 const adminPagosController ={};
 
-    adminPagosController.getInfoPagos = (req,res)=>{ res.send('Obten Pagos') };
+const PagosModel = require('../Models/pagos');
+
+    adminPagosController.getInfoPagos = async (req,res)=>{
+         //res.send('Obten Pagos');
+         try {
+
+            const pagos = await PagosModel.find();
+            res.json(pagos);
+             
+         } catch (error) {
+             console.error(error);
+         }
+    };
     adminPagosController.inserInfoPagos = (req,res)=>{ res.send('Realiza pago') };
 
 module.exports = adminPagosController;
--- server/src/Models/pagos.js
+++ server/src/Models/pagos.js
@@ -1,9 +1,14 @@
 const {Schema,model} = require('mongoose');
 
-new Schema({
-    departamento:{type:int,require:true},
-    monto:{type:doble,require:true},
-    departamento:{type:int,require:true},
-    departamento:{type:int,require:true},
-    departamento:{type:int,require:true},
+const PagosSchema = new Schema({
+    departamento:{type:Number,require:true},
+    monto:{type:Number,require:true},
+    Inicio_Periodo:{type:Date,require:true},
+    departamentFin_Peridoo:{type:Date,require:true},
+    pagado:{type:Boolean,require:true},
+},{
+    timestamps:true,
+    versionKey:false
 });
+
+module.exports = model('Pagos',PagosSchema);
