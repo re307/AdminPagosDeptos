171559f0afada4d075d7757b33253ee30f70004c
Rafael Eduardo Méndez <raed307@gmail.com>
2022-01-29 13:40:26 -0600

    APD - Termina servidor
    Se termina el servidor y se pasa a la vista con angular.

--- server/src/Controlles/adminPagos.controller.js
+++ server/src/Controlles/adminPagos.controller.js
@@ -13,19 +13,32 @@ const PagosModel = require('../Models/pagos');
              console.error(error);
          }
     };
+
     adminPagosController.insertInfoPagos = async (req,res)=>{
         const newPago = new PagosModel(req.body);
         await newPago.save();
         res.send({message:"Nuevo Pago generado"});
 
     };
+
     adminPagosController.getInfoPagoDep = async(req,res)=>{
 
         console.log(req.params);
         const {Id} = req.params;
         const PagoDepto = await PagosModel.find({_id:Id});
-        const {departamento} = PagoDepto;
-        res.send({message:"Consultando Depto: "+departamento,Info:PagoDepto});
+        const {departamento} = PagoDepto[0];
+        res.send({message:"Consultando Depto: "+departamento,Info:PagoDepto[0]});
+
+    };
+
+    adminPagosController.updatePagoRealizado = async(req,res)=>{
+
+        console.log(req.params);
+        const {Id} = req.params;
+        const {body} = req;
+        await PagosModel.findByIdAndUpdate(Id,body);
+        
+        res.send({message:"Actualiza Pago Realizado Id: "+Id});
 
     };
 
--- server/src/Routers/principal.router.js
+++ server/src/Routers/principal.router.js
@@ -6,6 +6,7 @@ const adminPagosController = require('../Controlles/adminPagos.controller');
 router.get('/',adminPagosController.getInfoPagos);
 router.post('/',adminPagosController.insertInfoPagos);
 router.get('/:Id',adminPagosController.getInfoPagoDep);
+router.put('/:Id',adminPagosController.updatePagoRealizado);
 
 
 
